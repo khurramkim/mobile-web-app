171ba2f0ea01e3c1849358f4e177310593619e17
Khurram <khurram@besthivepk.com>
2018-12-07 19:52:05 +0500

    add todolist assignment

---
+++ React/assignment/todolist/precache-manifest.221aaffff85cf6a7ea4eb42c4cec7636.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4301422f39efdaeb46a8114028ec66db",
+    "url": "/static/media/cat.4301422f.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0dd63cd555d4056abbf2",
+    "url": "/static/js/main.0dd63cd5.chunk.js"
+  },
+  {
+    "revision": "cf785a9f0991516102d2",
+    "url": "/static/js/1.cf785a9f.chunk.js"
+  },
+  {
+    "revision": "0dd63cd555d4056abbf2",
+    "url": "/static/css/main.95d4b39c.chunk.css"
+  },
+  {
+    "revision": "cf785a9f0991516102d2",
+    "url": "/static/css/1.765fea38.chunk.css"
+  },
+  {
+    "revision": "789806a4c74ac6c3d3c1efcac79affc6",
+    "url": "/index.html"
+  }
+];
